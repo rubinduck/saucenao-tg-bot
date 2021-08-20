1db101326b3acbeec634abbb73e054759a768626
rubinduck <rubinduck@protonmail.com>
2021-08-20 11:27:58 +0300

    Replace json with yaml

--- saucenao-tg-bot/__main__.py
+++ saucenao-tg-bot/__main__.py
@@ -1,48 +1,27 @@
-# TODO replace json with yaml
 import os
-import json
 import logging
 from argparse import ArgumentParser
+
+import yaml
 
 from bot import SauceNaoBot
 
 
-def verify_dict(scheme: dict, dict_to_verify: dict):
-    """
-    Takes dict example sheme and checks if given dict matches.
-    Is not super flexible and doesn't cover edge cases but does the job
-    """
-    for key, scheme_value in scheme.items():
-        if key not in dict_to_verify:
-            raise MyValueError(f'Given dict dont have {key} key')
-        if type(dict_to_verify[key]) != type(scheme_value):
-            raise MyValueError(
-                f'{key} has type [{type(dict_to_verify[key]).__name__}] ' +\
-                f'insetead of [{type(scheme_value).__name__}]')
-        verified_value = dict_to_verify[key]
-        if type(scheme_value) == list and len(scheme_value) != 0:
-            scheme_list_obj_type = type(scheme_value[0])
-            if type(verified_value[0]) != scheme_list_obj_type:
-                raise MyValueError(
-                    f'{key} list object has type [{type(verified_value[0]).__name__}] ' +\
-                    f'instead of [{scheme_list_obj_type.__name__}]')
-        elif type(scheme_value) == dict:
-            verify_dict(scheme_value, verified_value)
-
-class MyValueError(ValueError):
-    message: str
-    def __init__(self, *args, **kargs):
-        if len(args) != 0:
-            self.message = args[0]
-        super(*args[1:], **kargs)
-
+def is_config_right(config: dict, config_scheme: dict) -> bool:
+    """checks if config mathes config_scheme"""
+    if config.keys() != config_scheme.keys():
+        return False
+    for key in config_scheme:
+        if type(config_scheme[key]) != type(config[]):
+            return False
+    return True
 
 def main():
     """Launch bot using json config file if"""
     logging.basicConfig(filename='logs.log', level=logging.INFO)
     arg_parser = ArgumentParser()
     arg_parser.add_argument("config_file_path", type=str,
-                            help="path to json file with config like example")
+                            help="path to yaml file with config like example")
     args = arg_parser.parse_args()
     config_file_path = args.config_file_path
 
@@ -51,16 +30,13 @@ def main():
         return
     with open(config_file_path, 'r') as file:
         try:
-            config = json.load(file)
-        except json.JSONDecodeError:
-            print('You provided incorrect json file')
-    with open('example_config.json', 'r') as file:
-        config_scheme = json.load(file)
-    try:
-        verify_dict(config_scheme, config)
-    except MyValueError as exception:
-        print('Your config dont match example one:')
-        print(exception.message)
+            config = yaml.safe_load(file)
+        except yaml.YAMLError:
+            print('You provided incorrect yaml file')
+    with open('example_config.yaml', 'r') as file:
+        config_scheme = yaml.safe_load(file)
+    if not is_config_right(config, config_scheme):
+        print('Provided config file dont match example one')
         return
 
     sauce_nao_bot = SauceNaoBot(*config.values())
